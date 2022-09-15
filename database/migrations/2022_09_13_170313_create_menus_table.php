497ccbbbfa9a499925699f01071c37413fc27046
andreaszico <zicoaritonang@gmail.com>
2022-09-15 21:59:47 +0700

    fix menu collapse

--- database/migrations/2022_09_13_170313_create_menus_table.php
+++ database/migrations/2022_09_13_170313_create_menus_table.php
@@ -20,27 +20,8 @@ return new class extends Migration
             $table->unsignedBigInteger('parent_id')->nullable();
 
             $table->timestamp('created_at', 0)->nullable();
-            $table->uuid('created_by');
             $table->timestamp('updated_at', 0)->nullable();
-            $table->uuid('updated_by')->nullable();
             $table->timestamp('deleted_at', 0)->nullable();
-            $table->uuid('deleted_by')->nullable();
-
-            $table->foreign('deleted_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
-
-            $table->foreign('updated_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
-
-            $table->foreign('created_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
-
         });
     }
 
--- resources/views/components/accordion-menu-item.blade.php
+++ resources/views/components/accordion-menu-item.blade.php
@@ -2,11 +2,11 @@
     <h2 class="menu accordion-header" id="heading-{{ $menu->slug() }}-{{ $menu->id}}">
         <div class="px-3 ps-0 py-0 border-0" >
             <div class="d-flex align-items-center w-100 justify-content-between">
-                <div class="m-0 w-100 h-100 p-3 menu accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                <div class="m-0 w-100 h-100 p-3 menu accordion-button" type="button" data-bs-toggle="collapse"
                     data-bs-target="#collapse-{{ $menu->slug() }}-{{ $menu->id}}" aria-expanded="false"
                     aria-controls="collapse-{{ $menu->slug() }}-{{ $menu->id}}"><p class="m-0 p-0 ms-3">{{ $menu->menu_name() }}</p></div>
                 <div class="d-flex gap-2">
-                    <a class="btn btn-primary" href="{{ route('menu.create', $menu->id) }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Add New Child Menu"><i class="bi bi-plus-circle-fill"></i></a>
+                    <a class="btn btn-primary ms-3" href="{{ route('menu.create', $menu->id) }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Add New Child Menu"><i class="bi bi-plus-circle-fill"></i></a>
                     <a href="{{ route('menu.edit', $menu->id) }}" class="btn btn-warning"><i
                             class="bi bi-pencil-square"></i></a>
                     <form action="{{ route('menu.destroy', $menu->id) }}" method="post">
@@ -19,9 +19,9 @@
         </div>
     </h2>
     @if ($menu->childs() !== null)
-        <div id="collapse-{{ $menu->slug() }}-{{ $menu->id}}" class="accordion-collapse collapse"
+        <div id="collapse-{{ $menu->slug() }}-{{ $menu->id}}" class="accordion-collapse collapse show"
             aria-labelledby="heading-{{ $menu->slug() }}-{{ $menu->id}}">
-            <div class="accordion-body pe-0 py-0 border-0 d-flex flex-column gap-2">
+            <div class="accordion-body pe-0 py-0 border-0 d-flex flex-column">
                 @foreach ($menu->childs() as $item)
                     <x-accordion-menu-item :menu="$item"/>
                 @endforeach
