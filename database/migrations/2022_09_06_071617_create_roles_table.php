c6ed95393a385e7336f0bb2ac5ba8f4dc8d0c543
putriaditya <putriaditya166@gmail.com>
2022-09-15 10:52:42 +0700

    table role

--- database/migrations/2022_09_06_071617_create_roles_table.php
+++ database/migrations/2022_09_06_071617_create_roles_table.php
@@ -17,27 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('role')->unique();
             $table->index(['role']);
-            $table->timestamp('created_at', 0)->nullable();
-            $table->uuid('created_by');
-            $table->timestamp('updated_at', 0)->nullable();
-            $table->uuid('updated_by')->nullable();
             $table->timestamp('deleted_at', 0)->nullable();
-            $table->uuid('deleted_by')->nullable();
 
-            $table->foreign('deleted_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
-
-            $table->foreign('updated_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
-
-            $table->foreign('created_by')
-                ->references('uuid')
-                ->on('users')
-                ->onCascade('delete');
         });
     }
 
