96e28a863386b923d0ffcb6c1daca95323d35d84
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-09-16 04:25:57 +0700

    fix error tags

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -81,7 +81,6 @@
     <script src="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.js"></script>
     <script src="assets/js/pages/datatables.js"></script>
     <script src="assets/js/bootstrap.js"></script>
-    <script src="assets/js/app.js"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
