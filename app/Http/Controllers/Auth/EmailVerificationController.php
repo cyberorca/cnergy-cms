a7d33e85d71859f3aa59b04665590f4f732d27a2
andreaszico <zicoaritonang@gmail.com>
2022-09-18 21:21:46 +0700

    setup email verification

---
+++ app/Http/Controllers/Auth/EmailVerificationController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+class EmailVerificationController extends Controller
+{
+    public function verify(EmailVerificationRequest $request)
+    {
+        $request->fulfill();
+
+        return 'success'; // <-- change this to whatever you want
+    }
+}
