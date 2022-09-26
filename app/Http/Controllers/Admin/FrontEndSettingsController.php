37a96fd97ec6c2c296b53712fa77ae5e4da8266d
chusnul nabila <chusnulnab@gmail.com>
2022-09-26 10:03:13 +0700

    fix API-Formatting

--- app/Http/Controllers/Admin/FrontEndSettingsController.php
+++ app/Http/Controllers/Admin/FrontEndSettingsController.php
@@ -21,21 +21,7 @@ class FrontEndSettingsController extends Controller
     public function index()
     {
         $menu_settings = FrontEndSetting::first();
-        $data = [
-            "title" => $menu_settings["title"],
-            "site_description" => $menu_settings["site_description"],
-            "address" => $menu_settings["address"],
-            "sosmed" => json_encode([
-                "facebook" => $menu_settings["facebook"],
-                "fb_app_id" => $menu_settings["facebook_app_id"],
-                "instagram" => $menu_settings["instagram"],
-                "youtube" => $menu_settings["youtube"],
-                "twitter" => $menu_settings["twitter"],
-                "twitter_username" => $menu_settings["twitter_username"],
-            ]),
-            "color" => $menu_settings["color"],
-        ];
-        return view('admin.menu.settings.index', compact('data'));
+        return view('admin.menu.settings.index', compact('menu_settings'));
     }
 
     /**
