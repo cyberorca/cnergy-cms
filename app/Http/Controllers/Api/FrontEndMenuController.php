9b306e24b9de4bd0b24ff7d0ff12ae6d2c6e06c7
NoraNekoIT <bunsmart18@gmail.com>
2022-09-26 16:25:39 +0700

    Api Setting Fe Menu

---
+++ app/Http/Controllers/Api/FrontEndMenuController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\FrontEndMenu;
+use Illuminate\Http\Request;
+
+class FrontEndMenuController extends Controller
+{
+    public function index()
+    {
+        $fe_menus = FrontEndMenu::whereNull('parent_id')->with(["childMenus"])->get();
+        $data = $this->convertDataToResponse($fe_menus);
+        return response()->json($data);
+    }
+
+    private function convertDataToResponse($dataRaw){
+        $data= $dataRaw->transform(function ($item, $key) {
+            return [
+                'id' => $item->id,
+                'parent' => $item->parent_id,
+                'title'=>$item->title,
+                'url'=>'/'.$item->slug,
+                'type'=>'link',
+                'target'=>'self',
+                'order'=>$item->order,
+                'position'=>json_decode($item->position),
+                'children'=>$this->convertDataToResponse($item->childMenus)
+            ];
+        });
+        return $data;
+    }
+}
