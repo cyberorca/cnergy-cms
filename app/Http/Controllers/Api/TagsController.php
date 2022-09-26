af5e0bc8f1a1b4ed60d9d2870827bff645263eb1
putriaditya <putriaditya166@gmail.com>
2022-09-26 16:00:58 +0700

    [Tag]API-Formatting

--- app/Http/Controllers/Api/TagsController.php
+++ app/Http/Controllers/Api/TagsController.php
@@ -47,43 +47,6 @@ class TagsController extends Controller
                 "last_update" => $tags->updated_at,
             ];
         }
-
-        /*$data = [ "data" =>
-            ([
-                "id" => $tags["id"],
-                "name" => $tags["tags"],
-                "description" => "",
-                "content" => null,
-                "meta_title"=> "",
-                "meta_keyword"=> "",
-                "meta_description"=> "",
-                "is_headline" => 0,
-                "is_recommended" => 0,
-                "is_smart_tag" => 0,
-                "slug" => $tags["slug"],
-                "status" => $tags["is_active"],
-                "display_tag"=> null,
-                "smart_tag_type"=> null,
-                "smart_tag_url"=> null,
-
-                "smart_tag" => [
-                    "id" => null,
-                    "name" => null,
-                    "url" => null,
-                ],
-
-                "related_creator" => [],
-                "related_tag" => [],
-
-                "image" => [
-                    "real" => "",
-                ],
-
-                "date_entry" => $tags["created_at"],
-                "last_update" => $tags["updated_at"],
-            ])
-        ];*/
-
         return response()->json($res);
 
     }
