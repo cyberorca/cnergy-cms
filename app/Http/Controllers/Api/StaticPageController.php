ce8c347d067a527afdb822eef97344517d8d6d57
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-10-25 14:56:14 +0700

    fix

--- app/Http/Controllers/Api/StaticPageController.php
+++ app/Http/Controllers/Api/StaticPageController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class StaticPageController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
         $staticPage = StaticPage::latest();
 
         if($request->get("slug")){
