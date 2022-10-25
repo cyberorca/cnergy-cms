40cfd8b7f4753ee14ef6b29d824998ac94aec740
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-10-25 14:38:30 +0700

    static page fix

--- app/Http/Controllers/Api/StaticPageController.php
+++ app/Http/Controllers/Api/StaticPageController.php
@@ -10,10 +10,23 @@ use Illuminate\Http\Request;
 class StaticPageController extends Controller
 {
     public function index(){
-        $staticPage = StaticPage::latest()
-            ->with(['users'])
-            ->paginate(10);
-        return response()->json(new StaticPageCollection($staticPage))
-            ->setStatusCode(200);
+        $staticPage = StaticPage::latest();
+
+        if($request->get("slug")){
+            $staticPage->where('slug', '=', $request->get('slug', ''));
+        }
+
+        $limit = $request->get('limit', 20);
+        if($limit > 20){
+            $limit = 20;
+        }
+        
+        return response()->json(new StaticPageCollection($staticPage->paginate($limit)->withQueryString()))->setStatusCode(200);
+        
+        // $staticPage = StaticPage::latest()
+        //     ->with(['users'])
+        //     ->paginate(10);
+        // return response()->json(new StaticPageCollection($staticPage))
+        //     ->setStatusCode(200);
     }
 }
