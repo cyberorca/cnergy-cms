ac21726be6489ed448a995d331ee9fe78191466f
chusnul nabila <chusnulnab@gmail.com>
2022-10-28 11:12:20 +0700

    fixing filtering api video

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\VideoCollection;
 use App\Models\News;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class VideoController extends Controller
 {
@@ -28,7 +29,8 @@ class VideoController extends Controller
      *     )
      * )
      */
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         /*order by published_at desc
         //by is_published
         //limit 10
@@ -36,58 +38,63 @@ class VideoController extends Controller
         //filter by id category
         //filter by headline 1|0
         */
-        // $video = News::with(['categories', 'tags'])->where('types','=','video')->latest();
-        
-        $video = News::with(['categories', 'tags','users'])
-            ->where('types','=','video')
-            ->where('is_published','=','1')
-            ->where('published_at','<=',now())
+
+        $video = News::with(['categories', 'tags', 'users'])
+            ->where('is_published', '=', '1')
+            ->where('published_at', '<=', now())
             ->latest('published_at');
 
+        $alltype = $request->get('alltype', 0);
+        if ($alltype == 0) {
+            $video->where('types', '=', 'video');
+        }
 
-        if($request->get("headline")){
+        if ($request->get('orderby')) {
+            if ($request->get('orderby') == 'news_date_publish-desc') {
+                $video->OrderBy('published_at', 'desc');
+            }
+            if ($request->get('orderby') == 'news_entry-asc') {
+                $video->OrderBy('created_at', 'asc');
+            }
+        }
+
+        if ($request->get("headline")) {
             $video->where('is_headline', '=', $request->get('headline', ''));
         }
 
-        if($request->get("category")){
+        if ($request->get("category")) {
             $video->where('category_id', '=', $request->get('category', ''));
         }
 
-        if($request->get("max_id")){
+        if ($request->get("max_id")) {
             $video->where('id', '<', $request->get('max_id', ''));
         }
 
-        if($request->get("editorpick")){
+        if ($request->get("editorpick")) {
             $video->where('editor_pick', '=', $request->get('editorpick', ''));
-        }
-        
-        $all = News::with(['categories', 'tags'])->orderby('id');
-        $alltype = $request->get('alltype', 1);
-        if($alltype = 1){
-            $all;
-        } else if ($alltype = 0){
-            $video;
         }
 
         $published = $request->get('published', 1);
-        if($published == 0){
+        if ($published == 0) {
             $video->where('is_published', '=', "0");
         }
 
-        if($request->get("sensitive")){
+        if ($request->get("sensitive")) {
             $video->where('is_verify_age', '=', $request->get('sensitive', ''));
         }
 
-        if($request->get("last_update")){
-            $video->where('update_at', '=', $request->get('last_update', ''));
+        if ($request->get('last_update')) {
+            $last_update = Carbon::parse(($request->get('last_update')))
+                ->toDateTimeString();
+            $video->where('updated_at', '>=', $last_update);
         }
 
         $limit = $request->get('limit', 10);
-        if($limit > 10){
+        if ($limit > 10) {
             $limit = 10;
         }
 
         return response()->json(new VideoCollection($video->paginate($limit)->withQueryString()));
-    }
 
+    }
 }
