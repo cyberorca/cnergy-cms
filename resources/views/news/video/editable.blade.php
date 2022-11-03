86ac5d86a22ce84ece846b513e3fee7b9ec90aaa
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-11-03 17:29:55 +0700

    merge

--- resources/views/news/video/editable.blade.php
+++ resources/views/news/video/editable.blade.php
@@ -1,105 +1,104 @@
 @extends('layout.app')
 
 @section('css')
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('assets/css/pages/image-uploader.css') }}" />
+<link rel="stylesheet" href="{{ asset('assets/extensions/choices.js/public/assets/styles/choices.css') }}">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" rel="stylesheet" />
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
+<style type="text/css">
+    .bootstrap-tagsinput {
+        width: 100%;
+    }
 
+    .bootstrap-tagsinput .tag {
+        margin-right: 2px;
+        color: white !important;
+        background-color: #38E54D;
+        padding: .2em .6em .3em;
+        font-size: 100%;
+        font-weight: 700;
+        vertical-align: baseline;
+        border-radius: .25em;
+    }
 
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('assets/css/pages/image-uploader.css') }}" />
-    <link rel="stylesheet" href="{{ asset('assets/extensions/choices.js/public/assets/styles/choices.css') }}">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/css/bootstrap-select.min.css">
-    <style type="text/css">
-        .bootstrap-tagsinput {
-            width: 100%;
-        }
+    .search_select_box select {
+        width: 100%;
+        ;
+    }
 
-        .bootstrap-tagsinput .tag {
-            margin-right: 2px;
-            color: white !important;
-            background-color: #38E54D;
-            padding: .2em .6em .3em;
-            font-size: 100%;
-            font-weight: 700;
-            vertical-align: baseline;
-            border-radius: .25em;
-        }
+    a[aria-expanded=true] .bi-chevron-down {
+        display: none;
+    }
 
-        .search_select_box select {
-            width: 100%;
-            ;
-        }
+    a[aria-expanded=false] .bi-chevron-up {
+        display: none;
+    }
 
-        a[aria-expanded=true] .bi-chevron-down {
-            display: none;
-        }
-
-
-        a[aria-expanded=false] .bi-chevron-up {
-            display: none;
-        }
-    </style>
+</style>
 @endsection
 
 @push('head')
 @endpush
 
 @section('body')
-    @if ($method === 'edit')
-        <form action="{{ route('video.update', $news->id) }}" method="post" enctype="multipart/form-data">
-        @else
-            <form action="{{ route('video.store') }}" method="post" enctype="multipart/form-data">
-    @endif
-    <section id="basic-vertical-layouts">
-        @csrf
-        <div class="d-flex justify-content-between gap-2">
-            <div class="col-8 ">
-                <div class="card">
-                    <div class="card-header"><span class="h4 text-capitalize">{{ $method }} News</span>
-                    </div>
-                    <div class="card-body d-flex flex-column gap-2">
-                        @if ($method === 'edit')
-                            @method('PUT')
-                        @endif
-
-                        <div class="form-group">
-                            <label for="title" class="mb-2">Title</label>
-                            <input type="text" class="form-control" id="title" name="title"
-                                placeholder="Enter Title " required
-                                @if ($method === 'edit') value="{{ $news->title }}" @endif />
-                        </div>
-                        <div class="form-group">
-
-                            <label for="synopsis" class="form-label mb-2">Synopsis</label>
-                            <textarea name="synopsis" class="form-control" id="synopsis" cols="30" rows="3" required
-                                placeholder="Enter Synopsis">
 @if ($method === 'edit')
-{{ $news->synopsis }}
-@endif
-</textarea>
+<form action="{{ route('video.update', $news->id) }}" method="post" enctype="multipart/form-data">
+    @else
+    <form action="{{ route('video.store') }}" method="post" enctype="multipart/form-data">
+        @endif
+        <section id="basic-vertical-layouts">
+            @csrf
+            <div class="d-flex justify-content-between gap-2">
+                <div class="col-8 ">
+                    <div class="card">
+                        <div class="card-header">
+                            <span class="h4 text-capitalize">{{ $method }} News</span>
                         </div>
-                        <div class="form-group">
-                            <label for="content" class="form-label">Content</label>
-                            <textarea name="content" class="my-editor form-control" id="content" cols="30" rows="10" required>
-                                                @if ($method === 'edit')
-{{ $news->content }}
-@endif
-                                             </textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="video" class="mb-2">Video Embed Code (HTML)</label>
+
+                        <div class="card-body d-flex flex-column gap-2">
                             @if ($method === 'edit')
-                                <input type="hidden" name="video_id" value="{{ $news->news_videos[0]['id'] }}">
+                            @method('PUT')
                             @endif
-                            <textarea name="video" id="video" class="form-control" cols="30" rows="3" required>
-@if ($method === 'edit')
-{{ $news->news_videos[0]['video'] }}
-@endif
-</textarea>
-                        </div>
-                        {{-- <div class="d-flex justify-content-end gap-3 mt-3">
+                            <div class="form-group">
+                                <label for="title" class="mb-2">Title</label>
+                                <input type="text" class="form-control" id="title" name="title"
+                                    placeholder="Enter Title " required @if ($method==='edit' )
+                                    value="{{ $news->title }}" @endif />
+                            </div>
+                            <div class="form-group">
+                                <label for="synopsis" class="form-label mb-2">Synopsis</label>
+                                <textarea name="synopsis" class="form-control" id="synopsis" cols="30" rows="3" required
+                                    placeholder="Enter Synopsis">
+                                @if ($method === 'edit')
+                                    {{ $news->synopsis }}
+                                @endif
+                            </textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="content" class="form-label">Content</label>
+                                <textarea name="content" class="my-editor form-control" id="content" cols="30" rows="10"
+                                    required>
+                                @if ($method === 'edit')
+                                    {{ $news->content }}
+                                @endif
+                            </textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="video" class="mb-2">Video Embed Code (HTML)</label>
+                                @if ($method === 'edit')
+                                <input type="hidden" name="video_id" value="{{ $news->news_videos[0]['id'] }}">
+                                @endif
+                                <textarea name="video" id="video" class="form-control" cols="30" rows="3" required>
+                            @if ($method === 'edit')
+                                {{ $news->news_videos[0]['video'] }}
+                            @endif
+                            </textarea>
+                            </div>
+                            {{-- <div class="d-flex justify-content-end gap-3 mt-3">
                             <a href="{{ route('video.index') }}" class="btn btn-light" data-bs-toggle="tooltip"
-                                data-bs-placement="top" title="Back to Table Rome">Back</a>
+                            data-bs-placement="top" title="Back to Table Rome">Back</a>
 
                             <button class="btn btn-primary" name="save" type="submit" data-bs-toggle="tooltip"
                                 value="publish" data-bs-placement="top" title="Create Role">Save
@@ -110,30 +109,30 @@
                 </div>
             </div>
             @include('components.other-settings-news')
-        </div>
+            </div>
 
-        </div>
-    </section>
+            </div>
+        </section>
     </form>
-@endsection
+    @endsection
 
-
-@section('javascript')
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
+    @section('javascript')
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
     <script>
-        $(document).ready(function(){
+        $(document).ready(function () {
             $("#tags").select2({
-                placeholder:'Select Tags',
+                placeholder: 'Select Tags',
                 allowClear: true,
                 ajax: {
                     url: "{{route('tag.index')}}",
                     dataType: 'json',
                     delay: 250,
-                    processResults: function({data}){
+                    processResults: function ({
+                        data
+                    }) {
                         return {
-                            results: $.map(data, function(item){
+                            results: $.map(data, function (item) {
                                 return {
                                     id: item.id,
                                     text: item.tags
@@ -144,12 +143,13 @@
                 }
             });
         });
-        </script>
 
-<script src="https://cdn.tiny.cloud/1/vadmwvgg5mg6fgloc7tol190sn52g6mrsnk0dguphazk7y41/tinymce/4/tinymce.min.js"
+    </script>
+
+    <script src="https://cdn.tiny.cloud/1/vadmwvgg5mg6fgloc7tol190sn52g6mrsnk0dguphazk7y41/tinymce/4/tinymce.min.js"
         referrerpolicy="origin"></script>
-        <script src="{{ asset('assets/js/pages/image-uploader.js') }}"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
+    <script src="{{ asset('assets/js/pages/image-uploader.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 
@@ -165,9 +165,10 @@
             ],
             toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image media",
             relative_urls: false,
-            file_browser_callback: function(field_name, url, type, win) {
-                var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName(
-                    'body')[0].clientWidth;
+            file_browser_callback: function (field_name, url, type, win) {
+                var x = window.innerWidth || document.documentElement.clientWidth || document
+                    .getElementsByTagName(
+                        'body')[0].clientWidth;
                 var y = window.innerHeight || document.documentElement.clientHeight || document
                     .getElementsByTagName('body')[0].clientHeight;
 
@@ -190,16 +191,17 @@
         };
 
         tinymce.init(editor_config);
+
     </script>
 
-<script src="{{ asset('assets/extensions/choices.js/public/assets/scripts/choices.js') }}"></script>
+    <script src="{{ asset('assets/extensions/choices.js/public/assets/scripts/choices.js') }}"></script>
     <script src="{{ asset('assets/js/pages/form-element-select.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
 
     <script>
-        $(function() {
+        $(function () {
             $('input')
-                .on('change', function(event) {
+                .on('change', function (event) {
                     var $element = $(event.target);
                     var $container = $element.closest('.example');
 
@@ -220,13 +222,15 @@
                 })
                 .trigger('change');
         });
+
     </script>
 
     <script src="/path/to/cdn/jquery.slim.min.js"></script>
     <script src="/path/to/js/jquery.dateandtime.js"></script>
     <script>
-        $(function() {
+        $(function () {
             $('.example').dateAndTime();
         });
+
     </script>
-@endsection
+    @endsection
