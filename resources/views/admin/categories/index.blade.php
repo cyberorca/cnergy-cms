6f2899e3dc046c8f068bcfe24bd76826f1b923c1
andreaszico <zicoaritonang@gmail.com>
2022-11-18 09:20:31 +0700

    fix category

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,76 +1,51 @@
 @extends('layout.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('assets/css/pages/menu.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/pages/menu.css') }}" />
+    <style>
+        .accordion-body:hover {
+            background: transparent !important;
+        }
+
+        .accordion *:hover {
+            background: transparent !important
+        }
+    </style>
 @endsection
 
 @section('body')
-<x-page-heading title="Category Config" subtitle="Manage category" />
-{{-- 
-    <div class="card">
-
-        <div class="card-header d-flex align-items-center justify-content-between">
-            <span class="h4">Category Search</span>
+    <x-page-heading title="Category Config" subtitle="Manage category" />
+    <section class="section">
+        <div class="card">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <span class="h4">Category List</span>
+                <a href="{{ route('category.create') }}" class="btn btn-primary">
+                    <i class="bi bi-plus-circle"></i>&nbsp;&nbsp;&nbsp;Add Category
+                </a>
+            </div>
+            <div class="card-body d-flex flex-column gap-2">
+                <button class="btn btn-success col-2 d-none" id="button-save-order"><i class="bi bi-save"></i>
+                    &nbsp;&nbsp;Save</button>
+                @include('components.sortable.sortable', ['list' => $categories])
+            </div>
         </div>
 
-        <div class="card-body">
-
-            <form class="row g-3" method="GET">
-
-                <div class="col-md-4">
-                    <label for="inputCategory" class="form-label">Category</label>
-                    <input name="inputCategory" id="category" placeholder="Category" type="text" class="form-control">
-                </div>
-
-                <div class="col-md-4">
-                    <label for="inputSlug" class="form-label">Slug</label>
-                    <input name="inputSlug" id="slug" placeholder="Slug" type="text" class="form-control">
-                </div>
-
-                <div class="col-md-4">
-                    <label for="inputState" class="form-label">Status</label>
-                    <select name="status" id="inputState" class="form-select">
-                        <option value="" selected>All</option>
-                        <option value="1">Active</option>
-                        <option value="2">Inactive</option>
-                    </select>
-                </div>
-
-                <div class="d-flex justify-content-end gap-3 mt-3">
-                    <button type="submit" class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" title="Category Search">
-                        <i class="bi bi-search"></i>&nbsp;&nbsp;&nbsp;Search
-                    </button>
-                        
-                    <a href="{{route('categories.index')}}" class="btn btn-light" data-bs-toggle="tooltip" data-bs-placement="top" title="Show All Category Data">
-                        <i class="bi bi-card-list"></i>&nbsp;&nbsp;&nbsp;Show All
-                    </a>
-                </div>
-
-            </form>
+    </section>
+    <div class="toast-container position-fixed bottom-0 end-0 p-3">
+        <div id="liveToast" class="toast fade hide" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header bg-success">
+                <strong class="me-auto text-white">Message</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                Hello, world! This is a toast message.
+            </div>
         </div>
-    </div> 
---}}
-<section class="section">
-
-    <div class="card">
-        <div class="card-header d-flex align-items-center justify-content-between">
-
-            <span class="h4">Category List</span>
-
-            <a href="{{ route('category.create') }}" class="btn btn-primary">
-                <i class="bi bi-plus-circle"></i>&nbsp;&nbsp;&nbsp;Add Category
-            </a>
-
-        </div>
-
-        <div class="card-body d-flex flex-column gap-2">
-            <x-accordion-menu :accordion="$categories" />
-        </div>
-
     </div>
-    
-</section>
 @endsection
 
 @section('javascript')
+    <script src="{{ asset('assets/extensions/toastify-js/src/toastify.js') }}"></script>
+    <script src="{{ asset('assets/js/sortable.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/sortable-item.js') }}"></script>
 @endsection
---
+++ resources/views/components/sortable/sortable.blade.php
@@ -0,0 +1,5 @@
+<div class="list-group-item accordion" id="accordion-list">
+    @foreach ($list as $item)
+        @include('components.sortable.sortable-item', ['item' => $item ])
+    @endforeach
+</div>
