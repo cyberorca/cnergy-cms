80abf1b66d4fb4db48dcb81ed74e2d862f264276
chusnul nabila <chusnulnab@gmail.com>
2022-11-21 09:37:56 +0700

    orderby

--- app/Http/Controllers/Api/InventoryManagementController.php
+++ app/Http/Controllers/Api/InventoryManagementController.php
@@ -35,7 +35,8 @@ class InventoryManagementController extends Controller
 {
     public function index(Request $request)
     {
-        $inventoryManagement = InventoryManagement::latest();
+        // $inventoryManagement = InventoryManagement::latest();
+        $inventoryManagement = InventoryManagement::orderBy('id');
 
         $type = $request->get('type');
         if($type){
