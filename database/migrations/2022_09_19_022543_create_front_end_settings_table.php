25dcf7e8231b8896263406bcadb2ad57fc4dc72e
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-11-23 05:34:42 +0700

    migration files

--- database/migrations/2022_09_19_022543_create_front_end_settings_table.php
+++ database/migrations/2022_09_19_022543_create_front_end_settings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('front_end_settings', function (Blueprint $table) {
             $table->id();
+            // General Settings
             $table->string('site_title', 100)->nullable();
             $table->string('site_description', 255)->nullable();
             $table->string('address', 255)->nullable();
@@ -27,13 +28,31 @@ return new class extends Migration
             $table->string('twitter_username', 255)->nullable();
             $table->string('youtube', 255)->nullable();
             $table->string('youtube_username', 255)->nullable();
+            // Visual Settings
             $table->string('site_logo', 255)->nullable();
             $table->string('favicon', 255)->nullable();
-            $table->json('token')->nullable();
             $table->string('accent_color', 255)->nullable();
+            // General Configurations
+            $table->longText('facebook_fanspage')->nullable();
+            $table->longText('advertiser_id')->nullable();
+            $table->longText('cse_id')->nullable();
+            $table->longText('gtm_id')->nullable();
+            $table->longText('robot_txt')->nullable();
+            $table->longText('ads_txt')->nullable();
+            $table->longText('embed_code_data_studio')->nullable();
+            // Generate Token
+            $table->json('token')->nullable();
+            // Logs
             $table->timestamp('created_at', 0)->nullable();
+            $table->uuid('created_by')->nullable();
             $table->timestamp('updated_at', 0)->nullable();
+            $table->uuid('updated_by')->nullable();
             $table->timestamp('deleted_at', 0)->nullable();
+            $table->uuid('deleted_by')->nullable();
+            // Relations
+            $table->foreign('created_by')->references('uuid')->on('users')->onCascade('delete');
+            $table->foreign('updated_by')->references('uuid')->on('users')->onCascade('delete');
+            $table->foreign('deleted_by')->references('uuid')->on('users')->onCascade('delete');
         });
     }
 
