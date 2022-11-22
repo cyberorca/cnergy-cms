93fcb56e460a2bf3b18667d64efeab23031f0d19
andreaszico <zicoaritonang@gmail.com>
2022-11-22 13:13:26 +0700

    fixing menu api

--- app/Http/Utils/FileFormatPath.php
+++ app/Http/Utils/FileFormatPath.php
@@ -27,7 +27,8 @@ class FileFormatPath
         $fullFileNameExtension = explode('.' . $this->file->getClientOriginalExtension(), $this->file->getClientOriginalName())[0];
         $fileName = strlen($fullFileNameExtension) > 150 ? substr($fullFileNameExtension, 150) : $fullFileNameExtension;
         // path_name_file = /news/2022/10/04/23423/zico-artonang.jpg
-        return  substr(time(), 5) . '/' . Str::slug($fileName) . '.' . $this->file->getClientOriginalExtension();
+        // path_name_file = /news/2022/10/04/23423-zico-artonang.jpg
+        return  substr(time(), 5) . '-' . Str::slug($fileName) . '.' . $this->file->getClientOriginalExtension();
     }
 
     public function getFullPathName(){
