28c02e37756035664a7fba4dbff5622b7a9feb7e
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-11-30 10:02:23 +0700

    migration fe setting

---
+++ database/migrations/2022_11_30_095629_add_column_to_front_end_settings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('front_end_settings', function (Blueprint $table) {
+            $table->longText('domain_name')->nullable();
+            $table->longText('domain_url')->nullable();
+            $table->longText('domain_url_mobile')->nullable();
+            $table->longText('logo_url')->nullable();
+            $table->longText('copyright')->nullable();
+            $table->longText('email_domain')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('front_end_settings', function (Blueprint $table) {
+            //
+        });
+    }
+};
