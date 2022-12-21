3d56b4ffe33f3f1b9efd15dd6963f0979495119f
NoraNekoIT <bunsmart18@gmail.com>
2022-12-21 08:08:09 +0700

    force login if is_Active = 0

--- app/Http/Middleware/UsersPermissionRoles.php
+++ app/Http/Middleware/UsersPermissionRoles.php
@@ -4,25 +4,39 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersPermissionRoles
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next,...$roles)
+    public function handle(Request $request, Closure $next, ...$active)
     {
 //        if (in_array(auth()->user()->roles['role'],$roles)){
 //            return $next($request);
 //        }
-        if (auth()->user()->roles['role']!=null){
-            return $next($request);
-        }
 
-        return redirect()->intended('/login');
+
+//        if (auth()->user() != null) {
+//            if (auth()->user()->is_active = $active) {
+//                return $next($request);
+//            }
+//        }
+
+        if (Auth::check() && Auth::User()->is_active == 0) {
+            Auth::logout();
+            return redirect()->to('/login')->withErrors(['error'=> 'Your session has expired because your status change.']);
+        }
+        elseif (auth()->user() != null) {
+            if (auth()->user()->is_active = $active) {
+                return $next($request);
+            }
+        }
+        return $next($request);
     }
 }
