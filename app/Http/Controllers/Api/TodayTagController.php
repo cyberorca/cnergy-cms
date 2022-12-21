6c31ca3fefc6a8632ce0e8f9f2624aa23652c577
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-12-21 23:28:26 +0700

    cache

--- app/Http/Controllers/Api/TodayTagController.php
+++ app/Http/Controllers/Api/TodayTagController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\TodayTag;
 use App\Http\Resources\TodayTagCollection;
+use Symfony\Component\HttpFoundation\Response;
+use App\Http\Resources\TodayTagResource;
 
 class TodayTagController extends Controller
 {
@@ -20,5 +22,23 @@ class TodayTagController extends Controller
     
         // return response()->json($todayTag->get());
         return response()->json(new TodayTagCollection($todayTag->paginate($limit)->withQueryString()));
+    }
+
+    public function show($id){
+        $filterId = TodayTag::whereNull('deleted_at')
+        ->where('id', $id)
+        ->first();
+
+        if ($filterId == null){
+            return response()->json(['message'=>'Today Tag Not Found'], Response::HTTP_NOT_FOUND);
+        }
+
+        $cacheKey = "todayTagDetail-$id";
+        if(!Cache::has($cacheKey)){
+            CacheStorage::cache($cacheKey, 'today-tag');
+            Cache::put($cacheKey, new TodayTagResource($filterId), now()->addDay());
+        }
+
+        return response()->json(Cache::get($cacheKey), Response::HTTP_OK);
     }
 }
