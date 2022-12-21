85e87c56cb269f852f2e950cdf19f7c129362484
leonardochristofer <leonardo.christofer2001@gmail.com>
2022-12-21 09:17:12 +0700

    code cleaning

--- database/migrations/2022_09_19_022543_create_front_end_settings_table.php
+++ database/migrations/2022_09_19_022543_create_front_end_settings_table.php
@@ -32,7 +32,14 @@ return new class extends Migration
             $table->string('site_logo', 255)->nullable();
             $table->string('favicon', 255)->nullable();
             $table->string('accent_color', 255)->nullable();
-            // General Configurations
+            // General Configurations - Domain Settings
+            $table->longText('domain_name')->nullable();
+            $table->longText('domain_url')->nullable();
+            $table->longText('domain_url_mobile')->nullable();
+            $table->longText('logo_url')->nullable();
+            $table->longText('copyright')->nullable();
+            $table->longText('email_domain')->nullable();
+            $table->json('image_info')->nullable();
             $table->longText('facebook_fanspage')->nullable();
             $table->longText('advertiser_id')->nullable();
             $table->longText('cse_id')->nullable();
