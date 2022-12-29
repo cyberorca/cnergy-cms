a3aaffff2a2477b112337b85d669964588391ee1
andreaszico <zicoaritonang@gmail.com>
2022-12-29 09:14:20 +0700

    fix

--- resources/views/news/video/editable.blade.php
+++ resources/views/news/video/editable.blade.php
@@ -70,12 +70,20 @@
                         <div class="form-group">
                             <label for="synopsis" class="form-label mb-2">Synopsis</label>
                             <textarea name="synopsis" placeholder="Enter Synopsis" class="form-control" id="synopsis" cols="30" rows="3"
-                                required>@if ($method === 'edit'){{ $news->synopsis }}@endif</textarea>
+                                required>
+@if ($method === 'edit')
+{{ $news->synopsis }}
+@endif
+</textarea>
                         </div>
 
                         <div class="form-group">
                             <label for="content" class="form-label">Content</label>
-                            <textarea name="content" class="my-editor form-control" id="content" cols="30" rows="20">@if ($method === 'edit'){{ $news->content }}@endif</textarea>
+                            <textarea name="content" class="my-editor form-control" id="content" cols="30" rows="20">
+@if ($method === 'edit')
+{{ $news->content }}
+@endif
+</textarea>
                         </div>
 
                         <div class="form-group">
@@ -84,7 +92,11 @@
                                 <input type="hidden" name="video_id" value="{{ $news->news_videos[0]['id'] }}">
                             @endif
                             <textarea name="video" id="video" placeholder="Paste Embed Code Here" class="form-control" cols="30"
-                                rows="3" required>@if ($method === 'edit'){{ $news->news_videos[0]['video'] }}@endif</textarea>
+                                rows="3" required>
+@if ($method === 'edit')
+{{ $news->news_videos[0]['video'] }}
+@endif
+</textarea>
                         </div>
                     </div>
                 </div>
@@ -98,6 +110,7 @@
 @endsection
 
 @section('javascript')
+    <script src="{{ asset('assets/extensions/toastify-js/src/toastify.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
     <script>
@@ -293,7 +306,7 @@
                 .trigger('change');
         });
     </script>
-{{--
+    {{--
     <script src="/path/to/cdn/jquery.slim.min.js"></script>
     <script src="/path/to/js/jquery.dateandtime.js"></script>
 
